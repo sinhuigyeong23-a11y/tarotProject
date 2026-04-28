1e2f3abc0cd02c4d800b8d8db44394fb13121eed
sinhuigyeong23-a11y <sinhuigyeong23@gmail.com>
2026-04-28 17:36:17 +0900

    first commit

---
+++ tarot/src/main/java/com/project/tarot/TarotApplication.java
@@ -0,0 +1,13 @@
+package com.project.tarot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarotApplication.class, args);
+	}
+
+}
---
+++ tarot/src/test/java/com/project/tarot/TarotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tarot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
